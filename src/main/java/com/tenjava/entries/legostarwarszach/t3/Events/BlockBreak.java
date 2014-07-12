348e67ad4b4c31ffbaaa4772007b1efa69ed721a
Legostarwarszach <zfarley2123@gmail.com>
2014-07-12 13:37:22 -0400

    When grass breaks spawns chicken

--- src/main/java/com/tenjava/entries/legostarwarszach/t3/Events/BlockBreak.java
+++ src/main/java/com/tenjava/entries/legostarwarszach/t3/Events/BlockBreak.java
@@ -1,14 +1,28 @@
 package com.tenjava.entries.legostarwarszach.t3.Events;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreak implements Listener{
 	
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
 	public void onBlockBreak(BlockBreakEvent e){
-		
+		if(e.getPlayer() instanceof Player){
+			Player p = e.getPlayer();
+			Block b = e.getBlock();
+			if(b.getType() == Material.DIAMOND){
+				p.getWorld().strikeLightningEffect(b.getLocation());
+				return;
+			}if(b.getType() == Material.GRASS){
+				p.getWorld().spawnCreature(b.getLocation(), CreatureType.CHICKEN);
+				return;
+			}
+		}
 	}
 
 }
