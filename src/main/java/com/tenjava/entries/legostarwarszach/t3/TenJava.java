ba90cb18911dcdbe522cd1fef80a16a154fe9c1c
Legostarwarszach <zfarley2123@gmail.com>
2014-07-12 13:25:26 -0400

    Added onEnable and onDisable.

--- src/main/java/com/tenjava/entries/legostarwarszach/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/legostarwarszach/t3/TenJava.java
@@ -2,4 +2,20 @@ package com.tenjava.entries.legostarwarszach.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	public static double version = 1.0;
+	
+	@Override
+	public void onEnable() {
+		System.out.print("Enableing TenJava Entry for Legostarwarszach v." + version + "");
+		System.out.print("Enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.print("Disableing TenJava Entry for Legostarwarszach v." + version + "");
+		System.out.print("Disabled!");
+	}
+	
+}
