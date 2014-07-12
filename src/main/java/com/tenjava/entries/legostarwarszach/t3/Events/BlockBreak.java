531202d01fab3285419e39fc088c41784f6b1afb
Legostarwarszach <zfarley2123@gmail.com>
2014-07-12 13:25:47 -0400

    Added Class

---
+++ src/main/java/com/tenjava/entries/legostarwarszach/t3/Events/BlockBreak.java
@@ -0,0 +1,14 @@
+package com.tenjava.entries.legostarwarszach.t3.Events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BlockBreak implements Listener{
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent e){
+		
+	}
+
+}
